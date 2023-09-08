803b4f503150d3e1a1add89f704e4176e46493d8
Omar Abou Mrad <omar.aboumrad@gmail.com>
2023-09-08 21:32:59 +0300

    cleanups and readme updates

--- README.md
+++ README.md
@@ -1,5 +1,55 @@
 # git-heatmap
 
-Git cli heatmap
+Shows a heatmap for your Git repositories.
 
-TODO: add more
+![Screenshot](screenshot.png)
+
+## Usage
+
+```
+Usage: git-heatmap [OPTIONS]
+
+Options:
+  -r, --repo TEXT       Path to git repository (can be relative)
+  -a, --author TEXT     Author email (default all authors)
+  -b, --branch TEXT     Branch (default all branches)
+  -s, --start TEXT      Start date (YYYY-MM-DD, defaults to current year
+                        start)
+  -e, --end TEXT        End date (YYYY-MM-DD, defaults to current year end)
+  -c, --character TEXT  Character to use for heatmap (defaults to ▧)
+  -sh, --shade TEXT     Color to use for heatmap (defaults to 0;255;0)
+  --help                Show this message and exit.
+```
+
+##  Examples
+
+Run `git-heatmap` ...
+
+```
+# in a directory that's already a git repository
+$ git-heatmap
+
+# on a repository elsewhere
+$ git-heatmap -r /path/to/repo
+
+# limit by author email
+$ git-heatmap -a me@myself.com
+
+# pick a specific branch
+$ git-heatmap -b main
+
+# pick multiple branches
+$ git-heatmap -b main -b develop
+
+# start from date
+$ git-heatmap -s 2023-02-01
+
+# end on date
+$ git-heatmap -s 2023-02-28
+
+# change characters
+$ git-heatmap -c '●'
+
+# change colors
+$ git-heatmap -sh "255;255;0"
+```
--- src/main.py
+++ src/main.py
@@ -5,10 +5,11 @@ import collections
 import click
 import git
 
-Year = collections.namedtuple('Range', ['start', 'end'])(
+Year = collections.namedtuple("Range", ["start", "end"])(
     start=datetime.date.today().replace(month=1, day=1),
-    end=datetime.date.today().replace(month=12, day=31)
+    end=datetime.date.today().replace(month=12, day=31),
 )
+
 
 def generate_unique_commits(path, author, branches, start, end):
     """
@@ -19,7 +20,7 @@ def generate_unique_commits(path, author, branches, start, end):
     try:
         repo = git.repo.Repo(path)
     except git.exc.InvalidGitRepositoryError:
-        print(f'Invalid git repository: {path}', file=sys.stderr)
+        print(f"Invalid git repository: {path}", file=sys.stderr)
         sys.exit(1)
 
     target_branches = branches or repo.branches
@@ -61,17 +62,26 @@ def group_by_date(days):
 
 def generate_heatmap(repo, author, branches, start, end, density_map):
     """
-    Generates a heatmap
-        between: start and end
-        for: repository
-        density: map total commits to known density
+    Generates a heatmap between [start] and [end] for a [repo]
+    and [author] on [branches] using [density_map]
+
+    Result is a List[List[int]] where each row denotes a day of week
+    starting from Sunday and ending on Saturday. Values are between
+    0 and 4, where 0 is no commits and 4 is the highest density. The
+    map is responsible for mapping the number of commits to a density.
+
+    [
+        [0, 0, 0, 0, 0, 0, 0, ...],
+        [0, 0, 0, 0, 0, 0, 0, ...],
+        [0, 0, 0, 0, 0, 0, 0, ...],
+        [0, 0, 0, 0, 0, 0, 0, ...],
+        [0, 0, 0, 0, 0, 0, 0, ...],
+        [0, 0, 0, 0, 0, 0, 0, ...],
+        [0, 0, 0, 0, 0, 0, 0, ...],
+    ]
     """
     unique_commits = generate_unique_commits(
-        path=repo,
-        author=author,
-        branches=branches,
-        start=start,
-        end=end
+        path=repo, author=author, branches=branches, start=start, end=end
     )
 
     grouped_commits = group_by_date(unique_commits)
@@ -90,47 +100,32 @@ def generate_heatmap(repo, author, branches, start, end, density_map):
             heatmap[i].append(0)
 
     while current_day <= end:  # O(n)
-
         # This particular block will be used to calculate
         # where a specific month label (Jan, Feb...) should
         # start.
-        month_abbrev = current_day.strftime('%b')
+        month_abbrev = current_day.strftime("%b")
         if current_day == start:
             month_start_columns.append((0, month_abbrev))
         elif current_day.day == 1:
             month_start_columns.append((len(heatmap[day_of_week]), month_abbrev))
 
         value = grouped_commits.get(current_day, 0)
-        density = next(v for func, v in density_map if func(value))
+        density = next(idx for idx, func in enumerate(density_map) if func(value))
         heatmap[day_of_week].append(density)
         current_day += datetime.timedelta(days=1)
         day_of_week = (current_day.weekday() + 1) % 7
 
-    return heatmap #, month_start_columns
+    return heatmap
 
 
 def visualize(heatmap, color_map, character):
     """Heatmap visualization"""
-    # def show_months(month_start_columns, max_columns, character):
-    #     left_padding = 3  # Size of the day column
-    #     print(" " * left_padding, end="")
-    #     skip = 0
-    #     for i in range(max_columns):
-    #         months = [month for idx, month in month_start_columns if idx == i]
-    #         if months:
-    #             print(months[0], end='')
-    #             skip = 2
-    #         elif skip > 0:
-    #             skip -= 1
-    #         else:
-    #             print(' ' * len(character), end='')
-    #
-    #     print('')
-    #
-    # heatmap, month_start_columns = heatmap
-    # show_months(month_start_columns, len(heatmap[0]), character)
 
-    days = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa']
+    # TODO: Add month labels as first row
+    # generate_heatmap already collects this
+    # in month_start_columns variable.
+
+    days = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"]
     for day, row in zip(days, heatmap):
         print(day, end=" ")
         for value in row:
@@ -139,6 +134,7 @@ def visualize(heatmap, color_map, character):
 
 
 def vali_date(ctx, param, value):
+    """Validates the date strings and converts them to date"""
     if isinstance(value, tuple):
         return value
 
@@ -149,20 +145,20 @@ def vali_date(ctx, param, value):
 
 
 def amplify(rgb, rate):
-    rgb = [int(x) for x in rgb.split(';')]
-    return ';'.join(str(int(x * rate)) for x in rgb)
+    """Amplify the color by a rate"""
+    rgb = [int(x) for x in rgb.split(";")]
+    return ";".join(str(int(x * rate)) for x in rgb)
 
 
 @click.command()
-@click.option('--repo', '-r', default='.', help='Path to git repository (can be relative)')
-@click.option('--author', '-a', default=None, help='Author email (default all authors)')
-@click.option('--branch', '-b', default=[], multiple=True, help='Branch (default all branches)')
-@click.option('--start', '-s', default=Year.start, callback=vali_date, help='Start date (YYYY-MM-DD, defaults to current year start)')
-@click.option('--end', '-e', default=Year.end, callback=vali_date, help='End date (YYYY-MM-DD, defaults to current year end)')
-@click.option('--character', '-c', default='▧', help='Character to use for heatmap (defaults to ▧)')
-@click.option('--shade', '-sh', default='0;255;0', help='Color to use for heatmap (defaults to 0;255;0)')
+@click.option("--repo", "-r", default=".", help="Path to git repository (can be relative)")
+@click.option("--author", "-a", default=None, help="Author email (default all authors)")
+@click.option("--branch", "-b", default=[], multiple=True, help="Branch (default all branches)")
+@click.option("--start", "-s", default=Year.start, callback=vali_date, help="Start date (YYYY-MM-DD, defaults to current year start)")
+@click.option("--end", "-e", default=Year.end, callback=vali_date, help="End date (YYYY-MM-DD, defaults to current year end)")
+@click.option("--character", "-c", default="▧", help="Character to use for heatmap (defaults to ▧)")
+@click.option("--shade", "-sh", default="0;255;0", help="Color to use for heatmap (defaults to 0;255;0)")
 def run(repo, author, branch, start, end, character, shade):
-
     visualize(
         heatmap=generate_heatmap(
             repo=repo,
@@ -171,23 +167,24 @@ def run(repo, author, branch, start, end, character, shade):
             start=start,
             end=end,
             density_map=[
-                (lambda x: not x, 0),
-                (lambda x: 1 <= x < 3, 1),
-                (lambda x: 3 <= x < 6, 2),
-                (lambda x: 6 <= x < 8, 3),
-                (lambda x: 8 <= x, 4),
-            ]
+                lambda x: not x,
+                lambda x: 1 <= x < 3,
+                lambda x: 3 <= x < 6,
+                lambda x: 6 <= x < 8,
+                lambda x: 8 <= x,
+            ],
         ),
+        # TODO: Make this cleaner.
         color_map=[
             f"\033[1m",  # default
-            f"\033[38;2;{amplify(shade, 1)}m",    # bright
+            f"\033[38;2;{amplify(shade, 1)}m",  # bright
             f"\033[38;2;{amplify(shade, 0.7)}m",  # darker
             f"\033[38;2;{amplify(shade, 0.4)}m",  # very dark
             f"\033[38;2;{amplify(shade, 0.2)}m",  # darkest
         ],
-        character=f'{character} '
+        character=f"{character} ",
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
