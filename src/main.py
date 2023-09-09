f36794a23eb6001a0f63a9ac3666e6458f53f1be
Omar Abou Mrad <omar.aboumrad@gmail.com>
2023-09-09 21:11:56 +0300

    support for multiple repos and authors
    also drops the uniqueness check since
    git-rev-list doesn't return duplicates

--- src/main.py
+++ src/main.py
@@ -12,39 +12,31 @@ class Year:
 
 
 def generate_unique_commits(path, author, branches, start, end):
-    """
-    Load unique commits from a git repository.
-    Uniqueness is identified by the commit hash,
-    obviously across all branches.
-    """
+    """Load unique commits from a git repository."""
     try:
         repo = git.repo.Repo(path)
     except git.exc.InvalidGitRepositoryError:
         print(f"Invalid git repository: {path}", file=sys.stderr)
         sys.exit(1)
 
-    target_branches = branches or repo.branches
-    seen = set()
-
-    for branch in target_branches:
-        # TODO: branches param to iter_commits doesn't
-        # work as expected.
-        for commit in repo.iter_commits(
-            branch,
-            author=author,
-            since=start,
-            until=end,
-        ):
-
-            if commit.hexsha in seen:
-                continue
-            else:
-                yield commit.authored_datetime.date()
-
-            seen.add(commit.hexsha)
+    for commit in repo.iter_commits(
+        branches or repo.branches,
+        author=author,
+        since=start,
+        until=end,
+    ):
+        yield commit.authored_datetime.date()
 
 
-def generate_heatmap(repo, author, branches, start, end, density_map):
+def generate_for_repos(repos, author, branches, start, end):
+    """Generate unique commits for multiple repos"""
+    for repo in repos:
+        yield from generate_unique_commits(
+            path=repo, author=author, branches=branches, start=start, end=end
+        )
+
+
+def generate_heatmap(repos, author, branches, start, end, density_map):
     """
     Generates a heatmap between [start] and [end] for a [repo]
     and [author] on [branches] using [density_map]
@@ -64,8 +56,8 @@ def generate_heatmap(repo, author, branches, start, end, density_map):
         [0, 0, 0, 0, 0, 0, 0, ...],
     ]
     """
-    unique_commits = generate_unique_commits(
-        path=repo, author=author, branches=branches, start=start, end=end
+    unique_commits = generate_for_repos(
+        repos=repos, author=author, branches=branches, start=start, end=end
     )
 
     grouped_commits = collections.Counter(unique_commits)
@@ -102,13 +94,14 @@ def generate_heatmap(repo, author, branches, start, end, density_map):
     return heatmap
 
 
-def visualize(heatmap, color_map, character):
+def visualize(heatmap, start, end, color_map, character):
     """Heatmap visualization"""
 
     # TODO: Add month labels as first row
     # generate_heatmap already collects this
     # in month_start_columns variable.
 
+    print(f'{start.strftime("%Y-%m-%d")} => {end.strftime("%Y-%m-%d")}')
     days = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"]
     for day, row in zip(days, heatmap):
         print(day, end=" ")
@@ -135,8 +128,8 @@ def amplify(rgb, rate):
 
 
 @click.command()
-@click.option("--repo", "-r", default=".", help="Path to git repository (can be relative)")
-@click.option("--author", "-a", default=None, help="Author email (default all authors)")
+@click.option("--repo", "-r", default=["."], multiple=True, help="Path to git repository (can be relative)")
+@click.option("--author", "-a", default=[], multiple=True, help="Author email (default all authors)")
 @click.option("--branch", "-b", default=[], multiple=True, help="Branch (default all branches)")
 @click.option("--start", "-s", default=Year.start, callback=vali_date, help="Start date (YYYY-MM-DD, defaults to current year start)")
 @click.option("--end", "-e", default=Year.end, callback=vali_date, help="End date (YYYY-MM-DD, defaults to current year end)")
@@ -145,7 +138,7 @@ def amplify(rgb, rate):
 def run(repo, author, branch, start, end, character, shade):
     visualize(
         heatmap=generate_heatmap(
-            repo=repo,
+            repos=repo,
             author=author,
             branches=branch,
             start=start,
@@ -158,6 +151,8 @@ def run(repo, author, branch, start, end, character, shade):
                 lambda x: 8 <= x,
             ],
         ),
+        start=start,
+        end=end,
         # TODO: Make this cleaner.
         color_map=[
             f"\033[1m",  # default
